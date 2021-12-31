367800128affb76ec37d5bf9b7e3d44445a3afc8
B.Tuyen <79151413+Tuyen1612@users.noreply.github.com>
2021-12-31 12:53:44 +0700

    create project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _0306191373_0306191333_0306191376_0306191482.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
