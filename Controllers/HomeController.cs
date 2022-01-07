67072276af79541f878b30bac18786e394bacfd0
tien-it <69241666+tien-it@users.noreply.github.com>
2022-01-07 15:26:20 +0700

    load product

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,20 +7,31 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using _0306191373_0306191333_0306191376_0306191482.Data;
+
 namespace _0306191373_0306191333_0306191376_0306191482.Controllers
 {
     public class HomeController : Controller
-    {
+    {   //tiendv
+        private readonly ShopContext _context;
+        //tiendv
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ShopContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async  Task<IActionResult> Index()
         {
-            return View();
+            //tiendv
+            return View(await _context.Products.ToListAsync());
+            //tiendv
+           // return View();
+
         }
 
         public IActionResult Privacy()
